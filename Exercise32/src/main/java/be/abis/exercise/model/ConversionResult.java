6f15bf714597ef3257574e846109a7d9d5b8e717
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2024-02-01 14:26:03 +0100

    Exercise32

---
+++ Exercise32/src/main/java/be/abis/exercise/model/ConversionResult.java
@@ -0,0 +1,44 @@
+package be.abis.exercise.model;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+
+public class ConversionResult {
+
+	/*@JsonFormat(pattern="EEE',' dd MMM yyyy HH:mm:ss XXX")
+	private LocalDateTime time_last_update_utc;*/
+
+	private String time_last_update_utc;
+	private Map<String,Double> rates;
+
+	/*public Instant getTime_last_update_utc() {
+		return time_last_update_utc;
+	}
+
+	public void setTime_last_update_utc(Instant time_last_update_utc) {
+		this.time_last_update_utc = time_last_update_utc;
+	}*/
+
+	public String getTime_last_update_utc() {
+		return time_last_update_utc;
+	}
+
+	public void setTime_last_update_utc(String time_last_update_utc) {
+		this.time_last_update_utc = time_last_update_utc;
+	}
+
+	public Map<String, Double> getRates() {
+		return rates;
+	}
+
+	public void setRates(Map<String, Double> rates) {
+		this.rates = rates;
+	}
+}
---
+++ Exercise32/src/test/java/be/abis/exercise/CurrencyAPITest.java
@@ -0,0 +1,27 @@
+package be.abis.exercise;
+
+import be.abis.exercise.service.CurrencyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class CurrencyAPITest {
+
+    @Autowired
+    CurrencyService cs;
+
+
+    @Test
+    public void testGetRate() {
+        String fromCur="EUR";
+        String toCur="JPY";
+        double d = cs.getExchangeRate(fromCur,toCur);
+        System.out.println("Rate from " + fromCur + " to "+ toCur+  " is " + d);
+
+        assertEquals(1,cs.getExchangeRate(fromCur,fromCur));
+    }
+}
