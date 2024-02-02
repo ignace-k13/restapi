467013fe8fc6359af0d8135d56152239730a01ec
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2024-02-02 13:13:46 +0100

    Exercise43

---
+++ Exercise43api/src/main/java/be/abis/exercise/Exercise43apiApplication.java
@@ -0,0 +1,15 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+@SpringBootApplication
+public class Exercise43apiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise43apiApplication.class, args);
+    }
+
+}
